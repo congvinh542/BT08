ac2a30ad2bf7328fe87b709b8b3b3d5eb50e5ad7
congvinh542 <vinhdangcong375@gmail.com>
2024-10-08 16:36:24 +0700

    Add project files.

---
+++ Library.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+class Library
+{
+    public static int Bai01(int[] list01)
+    {
+        int result = -1;
+        foreach (var item in list01)
+        {
+            result += item;
+        }
+        return result;
+    }
+    public static int[] Bai02(int[] list02, int tagets)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < list02.Length; i++)
+        {
+            for (int j = i + 1; j < list02.Length; j++)
+            {
+                if (list02[i] + list02[j] == tagets)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+    public static int[] Bai03(int[] list03)
+    {
+        List<int> result = new List<int>();
+        foreach (var item in list03)
+        {
+            if (!result.Contains(item))
+            {
+                result.Add(item);
+            }
+        }
+        return result.ToArray();
+    }
+
+    public static int[] Bai04(int[] list04)
+    {
+        int count = 0;
+        List<int> result = new List<int>();
+        for(int i = 0; i < list04.Length; i++){
+            count = 0;
+            for (int j = 0; j < list04.Length; j++){
+                if(list04[i] == list04[j]){
+                    count++;
+                }
+            }
+            if (count > 1 && !result.Contains(list04[i]))
+            {
+                result.Add(list04[i]);
+            }
+        }
+        return result.ToArray();
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+#region BT01
+// int[] isNumber = {20,81,97,63,72,11,20,15,33,15,41,20};
+// Console.WriteLine($"Tổng của các số trong mảng isNumber: {Library.Bai01(isNumber)}");
+#endregion
+
+#region BT02
+// int[] Ist_number = { 2, 7, 11, 15 };
+// Console.WriteLine("Mời bạn nhập số tổng cần kiểm tra trong mảng");
+// int tagets = int.Parse(Console.ReadLine());
+// int[] result = Library.Bai02(Ist_number, tagets);
+// if (result != null)
+// {
+//     Console.WriteLine($"Hai số tại vị trí {result[0]} và {result[1]} có tổng bằng {tagets}");
+// }
+// else
+// {
+//     Console.WriteLine($"Không có cặp số nào có tổng bằng {tagets}");
+// }
+#endregion
+
+#region BT03
+// int[] IsNumber = {1,1,2,2,2,3,4,4,5};
+
+// System.Console.WriteLine($"Mảng sau khi xóa bỏ các phần từ trùng lặp: {String.Join(",", Library.Bai03(IsNumber))}");
+#endregion
+
+#region BT04
+/*int[] IsNumber = { 1, 1, 1, 2, 2, 3 };
+
+int[] result = Library.Bai04(IsNumber);
+if (result != null)
+{
+    Console.WriteLine($"Phần tử xuất hiện nhiều lần trong mảng là {string.Join(", ", result)}");
+}
+else
+{
+    Console.WriteLine($"Không có phần tử nào");
+}
+*/
+#endregion
+
+#region BT05
+
+#endregion
